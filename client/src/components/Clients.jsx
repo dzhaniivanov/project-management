6820824878616c15580452005f8d995695ecd6d1
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-08-30 18:23:03 +0300

    implemetn delete with delete mutation

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,21 +1,12 @@
 import { gql, useQuery } from "@apollo/client";
 import ClientRow from "./ClientRow";
-
-const GET_CLIENTS = gql`
-  query getClients {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`;
+import { GET_CLIENTS } from "../queries/clientQueries";
+import Spinner from "./Spinner";
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>loading...</p>;
+  if (loading) return <Spinner />;
   if (error) return <p>something went wrong...</p>;
 
   return (
